a9d3f7df7a535ea98cba6f8af28c69c3d3235b15
Nikki Murello <nmurello15@gmail.com>
2024-10-03 14:43:02 -0400

    Added more html content and mobile hamburger dropdown

--- scripts/dom_injector.js
+++ scripts/dom_injector.js
@@ -1,14 +1,12 @@
-
 function getNavbarHtml(relPrefix) {
     return `
-        <div class="bar-container menu-opener">
-            <a class="bar-link">
-                <i class="nf nf-md-menu"></i>
-            </a>
-        </div>
-
         <div class="bar-container">
             <h1 class="bar-title"><a href="${relPrefix}">not-phoeniix</a></h1>
+
+            <a href="javascript:void(0)" class="bar-link menu-opener" onclick="toggleSideNav()">
+                <i class="nf nf-md-menu"></i>
+            </a>
+
             <a class="bar-link" href="${relPrefix}about_me.html">About Me</a>
 
             <div class="dropdown">
@@ -16,9 +14,11 @@ function getNavbarHtml(relPrefix) {
                 <div class="dropdown-content">
                     <a href="${relPrefix}projects/goober_game.html">Goober Game</a>
                     <a href="${relPrefix}projects/watchfaces.html">Watchfaces</a>
+                    <a href="${relPrefix}projects/changeling.html">Changeling</a>
                     <a href="${relPrefix}projects/adumbration.html">Adumbration</a>
                 </div>
             </div>
+
         </div>
 
         <div class="bar-container">
@@ -36,16 +36,37 @@ function getNavbarHtml(relPrefix) {
 function getFooterHtml() {
     return `
         <div class="footer-links">
-            <p>Page by Nikki Murello :]</p>
+            <p>Coded by Nikki Murello :]</p>
             <p><a href="https://github.com/not-phoeniix/not-phoeniix.github.io" target="_blank">Source code</a></p>
         </div>
     `;
 }
 
+let navOpen = false;
+
+function toggleSideNav() {
+    navOpen = !navOpen;
+
+    const menuOpener = document.querySelector(".menu-opener");
+    const links = document.querySelectorAll(":not(.menu-opener).bar-link");
+
+    if (navOpen) {
+        menuOpener.innerHTML = `<i class="nf nf-oct-x"></i>`;
+        for (let link of links) {
+            link.style.display = "inline";
+        }
+    } else {
+        menuOpener.innerHTML = `<i class="nf nf-md-menu"></i>`;
+        for (let link of links) {
+            link.style.display = "none";
+        }
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-    let navHeader = document.querySelector(".navbar");
+    const navHeader = document.querySelector(".navbar");
     navHeader.innerHTML = getNavbarHtml(navHeader.dataset.relToRoot || "./");
 
-    let footer = document.querySelector("footer");
+    const footer = document.querySelector("footer");
     footer.innerHTML = getFooterHtml();
 });
